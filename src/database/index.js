f46643d985d4e9017a82d53b3fbdc58d4cc56711
Leonardo Tiago <pequenotiago2016@gmail.com>
2026-03-12 06:11:58 +0100

    Melhorando o index.js

--- src/database/index.js
+++ src/database/index.js
@@ -2,8 +2,15 @@ import { Sequelize } from 'sequelize';
 import databaseConfig from '../config/database.js';
 
 import User from '../models/User.js';
+import Bus from '../models/Bus.js';
+import Route from '../models/Route.js';
+import Schedule from '../models/Schedule.js';
+import Seat from '../models/Seat.js';
+import Ticket from '../models/Ticket.js';
+import Payment from '../models/Payment.js';
+import Report from '../models/Report.js';
 
-const models = [User];
+const models = [User, Bus, Route, Schedule, Seat, Ticket, Payment, Report];
 
 class Database {
   constructor() {
